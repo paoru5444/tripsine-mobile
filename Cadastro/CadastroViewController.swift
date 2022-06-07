a939a3b5c03fc50b66fd5b73975df1ce9f272e43
Alex Pacheco <alexrodpacheco@gmail.com>
2022-06-06 21:21:19 -0300

    Add classe CadastroViewController e funcionalidade de pickerViewController para armazenar foto no perfil

---
+++ Cadastro/CadastroViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CadastroViewController.swift
+//  tripsine-mobile
+//
+//  Created by Alex Rodrigues Pacheco on 06/06/22.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+    
+    let pickerController = UIImagePickerController()
+    
+    @IBOutlet var fotoPerfilImage: UIImageView!
+    
+    override func viewDidLoad() {
+        pickerController.delegate = self
+        super.viewDidLoad()
+    }
+    
+    @IBAction func AddFotoButton(_ sender: Any) {
+        pickerController.allowsEditing = true
+        pickerController.sourceType = .photoLibrary
+        
+        present(pickerController, animated: true)
+    }
+}
+
+extension CadastroViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.mediaType] as? UIImage {
+            fotoPerfilImage.image = image
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+}
+
