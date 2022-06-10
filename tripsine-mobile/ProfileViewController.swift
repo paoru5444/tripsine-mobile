d12d2062cdb55c95bc5eb4b926f69532bc81c2f4
BiancaButti <>
2022-06-10 18:44:40 -0300

    feat: navigation and dismiss button

--- tripsine-mobile/ProfileViewController.swift
+++ tripsine-mobile/ProfileViewController.swift
@@ -42,18 +42,7 @@ class ProfileViewController: UIViewController {
         profilePicImage.layer.masksToBounds = true
         profilePicImage.layer.cornerRadius = profilePicImage.bounds.width / 2
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- tripsine-mobile/RegisterViewController.swift
+++ tripsine-mobile/RegisterViewController.swift
@@ -13,19 +13,10 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        profileImage.layer.masksToBounds = true
-//        profileImage.layer.cornerRadius = profileImage.frame.width / 2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissPage(_ sender: Any) {
+        dismiss(animated: true)
     }
-    */
-
+    
 }
