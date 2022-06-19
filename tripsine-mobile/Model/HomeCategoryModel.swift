ec7b298477eae13d034cf167747cd0ed9cad5354
Paulo Roberto <paulo.pereira.dev@gmail.com>
2022-06-19 19:21:43 -0300

    feat: update home category model to add as reference of group

--- tripsine-mobile/Model/HomeCategoryModel.swift
+++ tripsine-mobile/Model/HomeCategoryModel.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
+//  HomeCategoryModel.swift
 //  tripsine-mobile
 //
-//  Created by Paulo Roberto on 19/06/22.
+//  Created by Bianca on 16/06/22.
 //
 
 import Foundation
+
+struct HomeCategoryModel: Codable {
+    let filters: SearchFilters
+    
+    enum CodingKeys: String, CodingKey {
+        case filters = "filters_v2"
+    }
+}
+
+struct SearchFilters: Codable {
+    let filterSection: [FilterSection]
+    
+    enum CodingKeys: String, CodingKey {
+        case filterSection = "filter_sections"
+    }
+}
+
+struct FilterSection: Codable {
+    let description: String
+    let section: String
+    
+    enum CodingKeys: String, CodingKey {
+        case description = "label"
+        case section = "section_id"
+    }
+}
