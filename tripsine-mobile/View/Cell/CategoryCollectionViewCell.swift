175b0210b6acfd54af8a28f98d4587e744d94a75
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2022-06-20 20:47:32 -0300

    fix: adjust images in assets

--- tripsine-mobile/View/Cell/CategoryCollectionViewCell.swift
+++ tripsine-mobile/View/Cell/CategoryCollectionViewCell.swift
@@ -13,7 +13,7 @@ enum CategoryImage: String {
     case meals = "meals"
     case estabileshimentType = "estabileshimentType"
     case miniumTravelRaiting = "miniumTravelRaiting"
-    case restaurantFeature
+    case restaurantFeature = "more"
     
 }
 
@@ -30,11 +30,12 @@ class CategoryCollectionViewCell: UICollectionViewCell {
             containerInsetView.backgroundColor = .white
             iconCategorieImage.image = UIImage(named: renderImageSection(section: filterSection[index].section))
             itemCategoryLabel.text = filterSection[index].description
+            iconCategorieImage.tintColor = .red
             itemCategoryLabel.numberOfLines = 0
             self.backgroundColor = .white
         } else {
             containerInsetView.backgroundColor = UIColor(red: 0.816, green: 0.067, blue: 0.063, alpha: 1)
-            iconCategorieImage.image = UIImage(named: "chef-hat-icon")
+            iconCategorieImage.image = UIImage(named: "chef-hat")
             itemCategoryLabel.text = filterSection.first?.description
             itemCategoryLabel.textColor = .white
             self.backgroundColor = .purple
@@ -66,7 +67,7 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         case "min_rating":
             return CategoryImage.miniumTravelRaiting.rawValue
         case "restaurant_features":
-            return String()
+            return CategoryImage.restaurantFeature.rawValue
         default:
             return String()
         }
